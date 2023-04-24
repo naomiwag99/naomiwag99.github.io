beff30cdd8153b8cb35eb93d5a124cc78d0ecad1
naomiwag99 <naog8805@colorado.edu>
2023-04-23 22:41:00 -0600

    Added project 13 along w other stuff

---
+++ js/projectNew.js
@@ -0,0 +1,23 @@
+var a=[
+    "Aloof",
+    "Arch",
+    "Authority",
+];
+
+var b=[
+    "Bear",
+    "Belly",
+    "Butterfly",
+];
+
+var c=[
+    "Circumstance",
+    "Complain",
+    "Cow"
+]
+
+var randa=Math.floor((Math.random() * a.length) );
+var randb=Math.floor((Math.random() * b.length) );
+var randc=Math.floor((Math.random() * c.length) );
+
+var acron=r[randa]+" "+b[randb]+" " + c[randc];
--- js/wa13.js
+++ js/wa13.js
@@ -1,72 +1,49 @@
+let x = null;
+let moreCounter = 0;
+let lessCounter = 0;
+let maxPressNum = Math.floor(Math.random() * 7) + 1; // set random max number of button presses
 
-
-function check() {
-    console.log('test');
-}
-
-function submit() {
-    alert('Your volume is now ' + output.textContent);
-}
-
-function reset() {
-    outputInt = 0;
-    output.textContent = outputInt;
-}
-
-function minus() {
-    if (outputInt > 0) {
-    outputInt -=1;
-    output.textContent = outputInt; }
-    
-}
-
-function plus() {
-    if (outputInt < 100) {
-    outputInt +=1;
-    output.textContent = outputInt;
-    }
-}
-
-function random() {
-    outputInt = randomNumber(0, 100);
-    output.textContent = outputInt;
-}
-
-function randomNumber(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+function startVolumeButton() {
+  if (x === null) {
+    x = Math.floor(Math.random() * 100) + 1;
+    let outputElement = document.getElementById("output");
+    let outputText = document.createTextNode("Your starting volume level is: " + x);
+    outputElement.appendChild(outputText);
+    document.getElementById("startB").disabled = true;
   }
-
-
-
-const output = document.querySelector('.output');
-let outputInt = parseInt(output.textContent);
-console.log(outputInt);
-
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
 }
-*/
 
-
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
-
-
-// Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
+function morePress() {
+  if (moreCounter < maxPressNum) {
+    const newStuff = Math.floor(Math.random() * (100 - x + 1)) + x;
+    document.getElementById("output").innerHTML = "Your next volume level is:  " + newStuff;
+    x = newStuff;
+    moreCounter++;
+  } else {
+    let outputElement = document.getElementById("output");
+    let outputText = document.createTextNode(". Please subscribe first to press the fun buttons more.");
+    outputElement.appendChild(outputText);
+    document.getElementById("moreB").disabled = true;
+    document.getElementById("lessB").disabled = true;
+  }
 }
+//
+function lessPress() {
+  if (lessCounter < maxPressNum) {
+    let newStuff = Math.floor(Math.random() * (x + 1));
+    document.getElementById("output").innerHTML = "Your new volume level is " + newStuff;
+    x = newStuff;
+    lessCounter++;
+  } else {
+    let outputElement = document.getElementById("output");
+    let outputText = document.createTextNode(". Please subscribe first to press the fun buttons more.");
+    //disables the buttons if its more than the count
+    outputElement.appendChild(outputText);    
+    document.getElementById("moreB").disabled = true;
+    document.getElementById("lessB").disabled = true;
+  }
+}
+
+document.getElementById("startB").addEventListener("click", startVolumeButton);
+document.getElementById("moreB").addEventListener("click", morePress);
+document.getElementById("lessB").addEventListener("click", lessPress);
